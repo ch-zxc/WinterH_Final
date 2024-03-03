0efa1912e1f780068eef64ce7ad131a8b6a72ff0
int_zxc <490749162@qq.com>
2024-03-03 11:05:19 +0800

    update data & readme

--- main.cpp
+++ main.cpp
@@ -5,13 +5,11 @@
 using namespace std;
 using namespace cv;
 
-Mat ans;
-
 Mat ROI_find(Rect RA, Rect R, Mat dilatemat, Rect ROI)
 {
     Mat ROI_Mat;
 
-    printf("RA:%d %d %d %d\nR:%d %d %d %d", RA.x, RA.y, RA.width, RA.height, R.x, R.y, R.width, R.height);
+    // printf("RA:%d %d %d %d\nR:%d %d %d %d", RA.x, RA.y, RA.width, RA.height, R.x, R.y, R.width, R.height);
 
     // Rect ROI(max(min(RA.x, R.x) - 50, 0), 
     //         max(min(RA.y, R.y) - 50, 0), 
@@ -25,7 +23,7 @@ Mat ROI_find(Rect RA, Rect R, Mat dilatemat, Rect ROI)
     return ROI_Mat;
 }
 
-void KFilter(Point RA, Point R , Mat measurement, KalmanFilter KF)
+Point KFilter(Point RA, Point R ,Mat measurement, KalmanFilter KF)
 {
     double center_x = (R.x + RA.x) / 2;
     double center_y = (R.y + RA.y) / 2;
@@ -37,12 +35,12 @@ void KFilter(Point RA, Point R , Mat measurement, KalmanFilter KF)
     measurement.at<float>(1) = (float)center_y;
     KF.correct(measurement);
 
-    circle(ans, predict_pt, 3, Scalar(34, 255, 255), -1);
+    //circle(ans, predict_pt, 3, Scalar(34, 255, 255), -1);
 
     center_x = (int)prediction.at<float>(0);
     center_y = (int)prediction.at<float>(1);
 
-    return;
+    return predict_pt;
 }
 
 Mat grphic_color_change(int mode, Mat imgOriginal)
@@ -50,14 +48,13 @@ Mat grphic_color_change(int mode, Mat imgOriginal)
     Mat imgHSV, imgBGR;
 	Mat imgThresholded;
 
-    if(0)
-    {
-        vector<Mat> hsvSplit;   //创建向量容器，存放HSV的三通道数据
-        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-        split(imgHSV, hsvSplit);			//分类原图像的HSV三通道
-        equalizeHist(hsvSplit[2], hsvSplit[2]);    //对HSV的亮度通道进行直方图均衡
-        merge(hsvSplit, imgHSV);				   //合并三种通道
-        cvtColor(imgHSV, imgBGR, COLOR_HSV2BGR);    //将HSV空间转回至RGB空间，为接下来的颜色识别做准备
+    if(0){
+        vector<Mat> hsvSplit;                           //创建向量容器，存放HSV的三通道数据
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);   //Convert the captured frame from BGR to HSV
+        split(imgHSV, hsvSplit);			            //分类原图像的HSV三通道
+        equalizeHist(hsvSplit[2], hsvSplit[2]);         //对HSV的亮度通道进行直方图均衡
+        merge(hsvSplit, imgHSV);				        //合并三种通道
+        cvtColor(imgHSV, imgBGR, COLOR_HSV2BGR);        //将HSV空间转回至RGB空间，为接下来的颜色识别做准备
     }else{
         imgBGR = imgOriginal.clone();
         imgThresholded = imgOriginal.clone();
@@ -69,15 +66,15 @@ Mat grphic_color_change(int mode, Mat imgOriginal)
         inRange(imgBGR, Scalar(0, 0, 78), Scalar(247, 255, 255), imgThresholded);
     }
 
-    for (int i = 0; i < imgThresholded.rows; ++i) {
-        for (int j = 0; j < imgThresholded.cols; ++j) {
+    for (int i = 0; i < imgThresholded.rows; ++i){
+        for (int j = 0; j < imgThresholded.cols; ++j){
             uchar pixel = imgThresholded.at<uchar>(i, j);
             
             // 判断当前像素是否为白色
             bool isWhitePixel = (pixel >= 250);
             
-            // 如果是蓝色像素则设置为黑色
-            if (isWhitePixel) {
+            // 反转效果 如果是白色像素则设置为黑色
+            if(isWhitePixel){
                 imgBGR.at<Vec3b>(i, j)[0] = 0;   // B通道
                 imgBGR.at<Vec3b>(i, j)[1] = 0;   // G通道
                 imgBGR.at<Vec3b>(i, j)[2] = 0;   // R通道
@@ -99,10 +96,10 @@ void pnp(RotatedRect pnp_rect)
 
     vector<Point3f> Points3D;
 
-    Points3D.push_back(Point3f(-67,-27.5,0));
-    Points3D.push_back(Point3f(-67,27.5,0));
-    Points3D.push_back(Point3f(67,-27.5,0));
-    Points3D.push_back(Point3f(67,27.5,0));
+    Points3D.push_back(Point3f(-67, -27.5, 0));
+    Points3D.push_back(Point3f(-67, 27.5, 0));
+    Points3D.push_back(Point3f(67, -27.5, 0));
+    Points3D.push_back(Point3f(67, 27.5, 0));
 
     //装甲板角点
     vector<Point2f> Points2D;
@@ -112,13 +109,13 @@ void pnp(RotatedRect pnp_rect)
     Points2D.push_back(pnp_vertices[4]); //2
 
     //SolvePnP
-    Mat rvec = Mat::zeros(3,1,CV_64F);
-    Mat tvec = Mat::zeros(3,1,CV_64F);
+    Mat rvec = Mat::zeros(3, 1, CV_64F);
+    Mat tvec = Mat::zeros(3, 1, CV_64F);
 
-    Mat rvec1 = Mat::zeros(3,1,CV_64F);
-    Mat tvec1 = Mat::zeros(3,1,CV_64F);
+    Mat rvec1 = Mat::zeros(3, 1, CV_64F);
+    Mat tvec1 = Mat::zeros(3, 1, CV_64F);
 
-    Mat_<double> intrinstic_matrix = (Mat_<double>(3,3) << 1000, 0 ,500, 0, 1000, 300, 0, 0, 1);
+    Mat_<double> intrinstic_matrix = (Mat_<double>(3, 3) << 1000, 0, 500, 0, 1000, 300, 0, 0, 1);
     Mat_<double> distortion_vec = (Mat_<double>(1, 5) << -0.1, 0.02, 0, 0, -0.001);
 
 
@@ -126,6 +123,7 @@ void pnp(RotatedRect pnp_rect)
     //                       InputArray cameraMatrix, InputArray distCoeffs,
     //                       OutputArray rvec, OutputArray tvec,
     //                       bool useExtrinsicGuess = false, int flags = SOLVEPNP_ITERATIVE );
+
     solvePnP(Points3D, Points2D, intrinstic_matrix, distortion_vec, rvec1, tvec1, false);
     //solvePnPRansec(Points3D, Points2D, intrinsic_matrix, distortion_vec, rvec, tvec, false, SOLVEPNP_AP3P)
     cout << rvec1 << " \n" << tvec1 << endl; 
@@ -137,7 +135,7 @@ int main()
 {
     int mode = 2;//mode:1=red 2=blue 
     VideoCapture video_source("../1.mp4"); 
-
+    Mat ans;
     double heights[16];
     int t = 0;
     bool flag = false;
@@ -154,20 +152,20 @@ int main()
         0, 0, 1, 0,
         0, 0, 0, 1);
 
-    setIdentity(KF.measurementMatrix);//H=[1,0,0,0;0,1,0,0] 测量矩阵
-    setIdentity(KF.processNoiseCov, Scalar::all(1e-5));//Q高斯白噪声，单位阵
-    setIdentity(KF.measurementNoiseCov, Scalar::all(1e-1));//R高斯白噪声，单位阵
-    setIdentity(KF.errorCovPost, Scalar::all(1));//P后验误差估计协方差矩阵，初始化为单位阵
-    randn(KF.statePost, Scalar::all(0), Scalar::all(0.1));//初始化状态为随机值 其实就是x_0
+    setIdentity(KF.measurementMatrix);                      //H=[1,0,0,0;0,1,0,0] 测量矩阵
+    setIdentity(KF.processNoiseCov, Scalar::all(1e-5));     //Q高斯白噪声，单位阵
+    setIdentity(KF.measurementNoiseCov, Scalar::all(1e-1)); //R高斯白噪声，单位阵
+    setIdentity(KF.errorCovPost, Scalar::all(1));           //P后验误差估计协方差矩阵，初始化为单位阵
+    randn(KF.statePost, Scalar::all(0), Scalar::all(0.1));  //初始化状态为随机值 其实就是x_0
     
     int ROI_time = 0, ROI_not_time = 0;
     bool ROI_mode = false;
 
-    for(int all = 0; ; ++all){
+    for (int all = 0; ; ++all){
         Mat sec, ROI_MAT, ROI_Back_MAT;
         int mark;
         video_source >> sec;    
-        imshow("sec",sec);
+        imshow("sec", sec);
         Mat source_Mat = sec;
         inRange(sec, Scalar(0, 0, 0), Scalar(0, 0, 0), source_Mat);
         inRange(source_Mat, Scalar(10, 10, 10), Scalar(10, 10, 10), source_Mat);
@@ -184,16 +182,16 @@ int main()
         //imshow("thresh_binary", thresh_binary);
 
         //腐蚀 & 膨胀
-        Mat dilatemat,dilatema;
+        Mat dilatemat, dilatema;
         dilate(thresh_binary, dilatema, getStructuringElement(MORPH_RECT, Size(2, 2)), Point(-1, -1), 3);
         erode(dilatema, dilatemat, getStructuringElement(MORPH_RECT, Size(2, 2)), Point(-1, -1));
-        imshow("dilatemat",dilatemat);
+        imshow("dilatemat", dilatemat);
         
         if(ROI_mode){
             Mat ROI_change = source_Mat.clone();
             
-            for(int i = ROI_final.x; i < ROI_final.x + ROI_final.width; ++i){
-                for(int j = ROI_final.y; j < ROI_final.y + ROI_final.height; ++j){
+            for (int i = ROI_final.x; i < ROI_final.x + ROI_final.width; ++i){
+                for (int j = ROI_final.y; j < ROI_final.y + ROI_final.height; ++j){
                     ROI_change.at<uchar>(j, i) = dilatemat.at<uchar>(j, i);
                     bool isWhitePixel = (dilatemat.at<uchar>(j, i) >= 250);
         
@@ -205,7 +203,7 @@ int main()
                 }
             }
             // Mat ROI_1 = dilatemat(Range(max(min(RA[mark].boundingRect().x, R[mark].boundingRect().x) - 50, 0), min(abs(RA[mark].boundingRect().x - R[mark].boundingRect().x) + max(RA[mark].boundingRect().width, R[mark].boundingRect().width) + 100, dilatemat.cols)), 
-            //                     Range(max(min(RA[mark].boundingRect().y, R[mark].boundingRect().y) - 50, 0), min(abs(RA[mark].boundingRect().x - R[mark].boundingRect().x) + max(RA[mark].boundingRect().width, R[mark].boundingRect().width) + 100, dilatemat.cols)));
+            // Range(max(min(RA[mark].boundingRect().y, R[mark].boundingRect().y) - 50, 0), min(abs(RA[mark].boundingRect().x - R[mark].boundingRect().x) + max(RA[mark].boundingRect().width, R[mark].boundingRect().width) + 100, dilatemat.cols)));
 
             // ROI_1.copyTo(ROI_change);
             
@@ -213,7 +211,7 @@ int main()
             dilatemat = ROI_change.clone();
             
         }
-        imshow("dilatemat after ROI",dilatemat);
+        imshow("dilatemat after ROI", dilatemat);
 
         vector<vector<Point>> contours;
         findContours(dilatemat, contours, RETR_LIST, CHAIN_APPROX_NONE);
@@ -221,7 +219,9 @@ int main()
         for (size_t i = 0; i < contours.size(); ++i){
             vector<Point> points;
             double area = contourArea(contours[i]);
-            if (area < 60 || 1e3 < area) continue;
+            if (area < 60 || 1e3 < area){
+                continue;
+            }
             drawContours(dilatemat, contours, static_cast<int>(i), Scalar(0), 2);
             
 
@@ -235,7 +235,7 @@ int main()
             // vector<vector<Point>> contours_ploy(contours.size());
             // vector<Rect> boundRect(contours.size());  
             
-            //画矩形
+            // 画矩形
             // for(int k = 0; k < contours.size(); ++k){
             //     //printf("%d\n",i);
             //     auto peri = arcLength(contours[k], true);
@@ -245,20 +245,25 @@ int main()
             //     //if(len(approx) != 4) continue;
             //     rectangle(ans, boundRect[k], Scalar(0, 0, 255), 2);
             // }    
-            for (int j = 0; j < 4; j++)
-            {
-                line(ans, vertices[j], vertices[(j + 1) % 4], Scalar(0, 0, 255),4);
+            for (int j = 0; j < 4; ++j){
+                line(ans, vertices[j], vertices[(j + 1) % 4], Scalar(0, 0, 255), 4);
             }
             high = rrect.size.height;
   
-            for(size_t j = 1; j < contours.size(); ++j){
+            for (size_t j = 1; j < contours.size(); ++j){
                 vector<Point> pointsA;
                 double area = contourArea(contours[j]);
                 double area2 = contourArea(contours[i]);
-                if(area / area2 > 2 || area / area2 < 0.5) continue;
+                if(area / area2 > 2 || area / area2 < 0.5){
+                    continue;
+                }
                 //if(area != area2) continue;
-                if(area2 < 20 || 1e3 < area2) continue;
-                if(area < 20 || 1e3 < area) continue;
+                if(area2 < 20 || 1e3 < area2){
+                    continue;
+                }
+                if(area < 20 || 1e3 < area){
+                    continue;
+                }
                 
                 double highA, distance, slop;
                 pointsA = contours[j];
@@ -267,7 +272,7 @@ int main()
 
                 slop = abs(rrect.angle - rrectA.angle);
                 highA = rrectA.size.height;
-                distance = sqrt((rrect.center.x-rrectA.center.x)*(rrect.center.x-rrectA.center.x) + (rrect.center.y-rrectA.center.y)*(rrect.center.y-rrectA.center.y));
+                distance = sqrt((rrect.center.x - rrectA.center.x) * (rrect.center.x - rrectA.center.x) + (rrect.center.y - rrectA.center.y) * (rrect.center.y - rrectA.center.y));
 
                 double max_height, min_height;
                 if(rrect.size.height > rrectA.size.height){
@@ -278,23 +283,22 @@ int main()
                     min_height = rrect.size.height;
                 }
 
-                //从学长那里参考来的装甲板筛选（
-                double line_x = abs(rrect.center.x-rrectA.center.x);//中心距离
+                double line_x = abs(rrect.center.x - rrectA.center.x);//中心距离
                 double difference = max_height - min_height;//长度差异
-                double aim =   distance/((highA+high)/2);//宽度差异
-                double difference3 = abs(rrect.size.width -rrectA.size.width);//
-                double height = (rrect.size.height+rrectA.size.height)/200;//
-                double slop_low = abs(rrect.angle + rrectA.angle)/2;//
+                double aim = distance / ((highA + high) / 2);//宽度差异
+                double difference3 = abs(rrect.size.width - rrectA.size.width);//
+                double height = (rrect.size.height + rrectA.size.height) / 200;//
+                double slop_low = abs(rrect.angle + rrectA.angle) / 2;//
                 
                 if((aim < 3.0 - height && aim > 2.0 - height 
                     && slop <= 5 && difference <= 8 && difference3 <= 5 
-                    &&(slop_low <= 30 || slop_low >=150) && line_x >0.6*distance)//小装甲板
+                    &&(slop_low <= 30 || slop_low >=150) && line_x > 0.6 * distance)//小装甲板
                     || (aim < 5.0 - height && aim > 3.2 - height 
-                    && slop <= 7 && difference <=15 && difference3 <= 8 
-                    && (slop_low <= 30 || slop_low >=150) && line_x >0.7*distance)){//大装甲板
+                    && slop <= 7 && difference <= 15 && difference3 <= 8 
+                    && (slop_low <= 30 || slop_low >= 150) && line_x > 0.7 * distance)){//大装甲板
 
                     //if(difference > 6) continue;
-                    heights[t] = (rrect.size.height+rrectA.size.height)/2;
+                    heights[t] = (rrect.size.height + rrectA.size.height) / 2;
                     R[t] = rrect;
                     RA[t] = rrectA;
                     t++;
@@ -305,8 +309,8 @@ int main()
 
         double maxx = 0;
         
-        for(int i = 0; i < t;i++){     //多个目标存在，打更近装甲板
-            if(heights[i]  >= maxx){
+        for (int i = 0; i < t; ++i){     //多个目标存在，打更近装甲板，效果有待商榷
+            if(heights[i] >= maxx){
                 maxx = heights[i];
                 mark = i;
             }
@@ -314,19 +318,19 @@ int main()
 
         // if(flag){
         //     for(int i = 0; i < t; ++i){
-
         //         circle(ans,Point((R[i].center.x + RA[i].center.x) / 2, (R[i].center.y + RA[i].center.y) / 2), 40, Scalar(255, 255, 0), 4);
         //         //string coordinatesText = to_string(static_cast<int>(i));
         //         //putText(ans, coordinatesText, Point((R[i].center.x + RA[i].center.x) / 2, (R[i].center.y + RA[i].center.y) / 2) , FONT_HERSHEY_COMPLEX, 0.5, Scalar(255, 255, 0), 1, 4);
         //         //
         //         //putText();
         //     }
-            
         // }
+
         if(flag){
-            circle(ans,Point((R[mark].center.x + RA[mark].center.x) / 2, (R[mark].center.y + RA[mark].center.y) / 2), 40, Scalar(255, 255, 0), 4);
-        
-            KFilter(R[mark].center, RA[mark].center, measurement, KF);
+            circle(ans, Point((R[mark].center.x + RA[mark].center.x) / 2, (R[mark].center.y + RA[mark].center.y) / 2), 40, Scalar(255, 255, 0), 4);
+
+            circle(ans, KFilter(R[mark].center, RA[mark].center, measurement, KF), 3, Scalar(34, 255, 255), -1);
+            //KFilter(R[mark].center, RA[mark].center, measurement, KF);
             
             Rect ROI_Rect(max(min(RA[mark].boundingRect().x, R[mark].boundingRect().x) - 25, 5), 
                         max(min(RA[mark].boundingRect().y, R[mark].boundingRect().y) - 25, 5), 
